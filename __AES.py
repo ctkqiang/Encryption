3e907e4d50ff497c9173e49b1bcb7f1cadfc9db5
John Melody Me <johnmelodyme@yandex.com>
2020-05-06 21:15:16 +0800

    Update __AES.py

--- __AES.py
+++ __AES.py
@@ -7,40 +7,37 @@ from cryptography.hazmat.backends import default_backend as _default_backend
 from cryptography.hazmat.primitives import hashes as _hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC as _PBKDF2HMAC
 
-class __AES_Algorithm:
 
-      __key = Fernet.generate_key()
 
-      @staticmethod
-      def main():
-            __input = input("Encrypt or Decrypt? [e/d]: ")
-            if __input.find("e"):
-                  __encryption()
-            elif __input.find("d"):
-                  __decryption()
-            else:
-                  print("Please Input A Valid Choice.")
+__key = Fernet.generate_key()
 
-      @staticmethod
-      def __decryption():
-            print("aa")
+def main():
+      __input = input("Encrypt or Decrypt? [e/d]: ")
+      if __input.find("e"):
+            __encryption()
+      elif __input.find("d"):
+            __decryption()
+      else:
+            print("Please Input A Valid Choice.")
 
-      @staticmethod
-      def __encryption():
-            __salt = b'salt_'
-            __user_input = bytes(input("[Input]: "), "utf8")
-            _KDF = _PBKDF2HMAC(
-                  algorithm = _hashes.SHA256(),
-                  length = 32,
-                  salt = __salt,
-                  iteration = 100000,
-                  backend = _default_backend()
-            )
-            __encrypted_String = __user_input.encode() # Convert to `bytes[]`
-            __key = base64.urlsafe_b64decode(_KDF.derive(__encrypted_String))
-            __file = open("exported_keys/_crypto_key.key", "wb")
-            __file.write("Key:", __key, "\n", "User Input: ", __user_input, "\n", "Encrypted: ", __encrypted_String)
-            __file.close()
+def __decryption():
+      print("aa")
+
+def __encryption():
+      __salt = b'salt_'
+      __user_input = bytes(input("[Input]: "), "utf8")
+      _KDF = _PBKDF2HMAC(
+            algorithm = _hashes.SHA256(),
+            length = 32,
+            salt = __salt,
+            iteration = 100000,
+            backend = _default_backend()
+      )
+      __encrypted_String = __user_input.encode() # Convert to `bytes[]`
+      __key = base64.urlsafe_b64decode(_KDF.derive(__encrypted_String))
+      __file = open("exported_keys/_crypto_key.key", "wb")
+      __file.write("Key:", __key, "\n", "User Input: ", __user_input, "\n", "Encrypted: ", __encrypted_String)
+      __file.close()
 
 if __name__ == "__main__":
-      __AES_Algorithm.main()
+      main()
